d97b532054ae56c0046483cbc2341018609d9fbf
Nicolas Gramlich <ngramlich@zynga.com>
2013-03-06 00:51:48 -0800

    Reduced any font size by 1.

--- src/org/vamp/Main.java
+++ src/org/vamp/Main.java
@@ -1,8 +1,12 @@
 package org.vamp;
 
+import java.util.Hashtable;
+import java.util.Map.Entry;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 import org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel;
 
@@ -29,13 +33,16 @@ public class Main {
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(new SubstanceGraphiteGlassLookAndFeel());
-				} catch (Exception e) {
+
+					/* Reduce font size a little: */
+					Main.changeFontSizeBy(-1);
+				} catch (final Exception e) {
 					e.printStackTrace();
 				}
 
 				final String mediaFilename = pArgs[0];
 
-				final String[] vlcArgs = new String[pArgs.length - 1 + 1];
+				final String[] vlcArgs = new String[(pArgs.length - 1) + 1];
 				System.arraycopy(pArgs, 1, vlcArgs, 0, pArgs.length - 1);
 				vlcArgs[vlcArgs.length - 1] = "--no-video-title-show";
 
@@ -60,6 +67,17 @@ public class Main {
 	// ===========================================================
 	// Methods
 	// ===========================================================
+
+	private static void changeFontSizeBy(final int pSizeReduction) {
+		final Hashtable<Object, Object> defaults = UIManager.getDefaults();
+		for (final Entry<Object, Object> entry : defaults.entrySet()) {
+			final Object key = entry.getKey();
+			if ((key instanceof String) && (((String) key).endsWith(".font"))) {
+				final FontUIResource font = (FontUIResource) UIManager.get(key);
+				defaults.put(key, new FontUIResource(font.getFontName(), font.getStyle(), font.getSize() + pSizeReduction));
+			}
+		}
+	}
 
 	// ===========================================================
 	// Inner and Anonymous Classes
